75094b26b6d45b756eb7a062b42d882619fe146b
Schneider <deanna.schneider@ces.uwex.edu>
2016-11-16 15:06:30 -0600

    Added uglify

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass');
+    gulpif = require('gulp-if');
+    uglify = require('gulp-uglify');
 
 var env,
     coffeeSources,
@@ -48,6 +50,7 @@ gulp.task('js', function(){
 gulp.src(jsSources)
 	.pipe(concat('script.js'))
 	.pipe(browserify())
+  .pipe(gulpif(env === 'production', uglify()))
 	.pipe(gulp.dest(outputDir + '/js'))
   .pipe(connect.reload())
 
